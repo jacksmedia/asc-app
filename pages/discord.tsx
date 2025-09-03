b422059c1911855e7b7e484c0d601db432fdb85a
jacksmedia <alex@j4cks.com>
2025-09-02 17:01:51 -0700

    🛠ditto

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -1,12 +1,12 @@
 import { NextPage } from 'next';
 import Layout from '@/layout';
-import ClassicTitle from "@/components/ClassicTitle";
+import TitleScreen from "@/components/TitleScreen";
 
 const Discord: NextPage = () => {
   return (
     <Layout>      
       <div className='discord-bg'>
-        <ClassicTitle />
+        <TitleScreen />
         <h1 className="">Discuss FF6 ASC</h1>
         <h2>#lightning-projects</h2>        
         <h2><a href="https://discord.gg/PGMASbSnD9" target='blank'>Join us in the FF4 Ultima Discord</a></h2>
